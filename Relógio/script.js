89d0d721f2a5a2638dca3ea4969b0ffd117d7319
carlos230300 <carllos2303@gmail.com>
2022-09-13 12:58:10 -0300

    Relogio
    Relógio em HTML, CSS e Java Script

---
+++ Relógio/script.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hor = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if(hor < 10) hor = '0' + hor;
+    if(min < 10) min = '0' + min;
+    if(seg < 10) seg = '0' + seg;
+
+    horas.textContent = hor;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+
+
+})
